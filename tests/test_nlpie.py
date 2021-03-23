2d52e46937b87bab5c156aaaaf4c6f172e4d9dfe
amrosnik <amrosnik@gmail.com>
2021-03-23 14:10:36 -0700

    added more NLP IE tests.

--- tests/test_nlpie.py
+++ tests/test_nlpie.py
@@ -2,6 +2,7 @@ import pytest
 from legiscrapor.nlpIE import *
 import os 
 import re 
+import legiscrapor.pdf_saver as ps
 
 @pytest.fixture
 def text_example():
@@ -33,3 +34,44 @@ def test_clean(text_example):
     assert ("\n " not in cleaned_text)
     assert ( len(re.findall('[0-9]+.\t',cleaned_text)) == 0 )
     assert ( len(re.findall("[\(\[].*?[\)\]]",cleaned_text)) == 0 )
+
+def test_sentences(text_example):
+    cleaned_text = clean(text_example) 
+    cleaned_sents = sentences(cleaned_text)
+    # the expected number of sentences in the doc is 18. 
+    assert len(cleaned_sents) == 18
+
+def test_subtree(text_example):
+    cleaned_text = clean(text_example) 
+    cleaned_sents = sentences(cleaned_text)
+    patterns = ["legal aid", "judicial assistance", "legal assistance", "legal service"]
+    subs = [sent_subtree(sentence,patterns) for sentence in cleaned_sents ] 
+
+    ## let's check that each sentence of cleaned_sents was tokenized correctly: 
+    assert len(subs[0]) == 42
+    assert len(subs[1]) == 25
+    assert len(subs[2]) == 35
+    assert len(subs[3]) == 0
+    assert len(subs[4]) == 90
+    assert len(subs[5]) == 0
+    assert len(subs[6]) == 0
+    assert len(subs[7]) == 74
+    assert len(subs[8]) == 42
+    assert len(subs[9]) == 0
+    assert len(subs[10]) == 71
+    assert len(subs[11]) == 80
+    assert len(subs[12]) == 106
+    assert len(subs[13]) == 0
+    assert len(subs[14]) == 7
+    assert len(subs[15]) == 0
+    assert len(subs[16]) == 13
+    assert len(subs[17]) == 0
+
+def test_full_nlp():
+    # unit tests for full_nlp_ie() 
+    df = ps.scan_pdfs('./src/legiscrapor/data/pdfsaver_docs/')
+    keywords = ["legal aid", "judicial assistance", "legal assistance", "legal service"]
+    stuff = full_nlp_ie(df,keywords,'English',1)
+    print(stuff)
+    assert 2 == 3
+
