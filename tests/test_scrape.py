6d8d90792e1aa43cd3682e86c34bdf2d0100359b
amrosnik <amrosnik@gmail.com>
2021-03-22 17:09:07 -0700

    added a test for selsearch.search_for_keywords(). really need to figure out how to assume a chromedriver for testing...

--- tests/test_scrape.py
+++ tests/test_scrape.py
@@ -3,7 +3,7 @@ import re
 from legiscrapor import selsearch
 import pandas as pd
 from webdriver_manager.chrome import ChromeDriverManager
-# test-driving Selenium on my own website.
+import pytest
 
 """
 # dataframe display settings
@@ -23,12 +23,33 @@ driver.get(code_href)
 
 ## next: scrape resulting page for specific text. Print the results.
 whole_about_page = driver.page_source
-keywords = ["absurd", "silly", "surreal"]  # none of these are on the page; total = 0
-keywords = ["R ", "San Francisco", "data "]  # all 3 of these appear on the page source code; totals = 3,4,3, total total=10
+"""
 
-counts = selsearch.search_for_keywords(whole_about_page, keywords)
-print(counts)
+@pytest.fixture
+def whole_page():
+    ''' Returns the HTML source code for a webpage on the India Legislative website, 
+    https://legislative.gov.in/about-us/about-departments '''
+    with open('./src/legiscrapor/data/legislative-dot-gov-dot-in__about-us__about-departments.html', 'r') as f:
+        webpage = f.read()
+    return webpage
 
+def test_find_keywords(whole_page):
+    ''' test for keywords that actually appear on the whole_page webpage '''
+    keywords = ["department", "ministry"]  
+    counts = selsearch.search_for_keywords(whole_page, keywords)
+    assert counts['department'] == 3
+    assert counts['ministry'] == 2
+    assert counts['TOTAL'] == 5
+
+def test_no_keywords(whole_page): 
+    ''' test for keywords that do NOT appear on the whole_page webpage '''
+    keywords = ["blue","cake"]  
+    counts = selsearch.search_for_keywords(whole_page, keywords)
+    assert counts['blue'] == 0
+    assert counts['cake'] == 0
+    assert counts['TOTAL'] == 0
+
+"""
 # SECOND TEST: click a link + search resulting page for links w/ certain words in xpath/link text.
 # then click on all links found and search for keyword matches.
 
@@ -41,7 +62,6 @@ n_elements = driver.find_elements_by_xpath('//a')
 
 # find the keyword matches
 matches = selsearch.find_keywords_from_links(driver, n_elements, pattern, keywords)
-
 
 driver.close()
 """
